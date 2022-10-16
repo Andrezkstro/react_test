a4fcb0d027b1e80b01e2361abd5c696940aa9d3d
andrezale <andrezkstro@gmail.com>
2022-10-16 12:35:03 -0500

    1610 No muestra nada

--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const App = () => {
-    return (
-        <h1>Hola Mundo!!! Como estas Total Test dario</h1>
-    );
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from './components/App'
-
-ReactDOM.render(<App />, document.getElementById('app'));
+import React from 'react';
+import App from './routes/App';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
